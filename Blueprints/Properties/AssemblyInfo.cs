1c9347aaa9fabf84955abc5451a3d58cac9aae51
Kaylem Wood <KaylemWood@outlook.com>
2019-08-31 23:56:33 +0100

    Removed GUID.

--- Blueprints/Properties/AssemblyInfo.cs
+++ Blueprints/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Blueprints")]
 [assembly: AssemblyCopyright("Copyright Mayall 2019")]
 [assembly: ComVisible(false)]
-[assembly: Guid("2d294e8e-129d-4784-8681-f6843850a598")]
-[assembly: AssemblyVersion("2.0.73")]
+[assembly: AssemblyVersion("2.0.82.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
--- WireCutter/Properties/AssemblyInfo.cs
+++ WireCutter/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Wire Cutter")]
 [assembly: AssemblyCopyright("Copyright Mayall 2019")]
 [assembly: ComVisible(false)]
-[assembly: Guid("2d294e8e-129d-4784-8681-f6843850a598")]
-[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
