0edc3ae578163225b9032eade3720bf7ee59c18e
Kaylem Wood <KaylemWood@outlook.com>
2019-09-02 15:56:13 +0100

    Incremented version.

--- WireCutter/Properties/AssemblyInfo.cs
+++ WireCutter/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Wire Cutter")]
 [assembly: AssemblyCopyright("Copyright Mayall 2019")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
