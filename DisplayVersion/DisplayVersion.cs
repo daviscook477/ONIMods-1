26ba41c52d01e562abaf91b33f099669f0ac5b97
Kaylem Wood <KaylemWood@outlook.com>
2019-09-06 23:19:27 +0100

    Added new display version library.

---
+++ DisplayVersion/DisplayVersion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+using Harmony;
+using UnityEngine;
+
+namespace DisplayVersion {
+    [HarmonyPatch(typeof(ModsScreen), "BuildDisplay")]
+    public static class ModsScreen_BuildDisplay {
+        public static LocText versionLocText;
+
+        public static void Postfix(Transform ___entryParent) {
+            string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            for (int i = 0; i < Global.Instance.modManager.mods.Count; ++i) {
+                KMod.Mod mod = Global.Instance.modManager.mods[i];
+
+                if ((mod.loaded_content & KMod.Content.DLL) == KMod.Content.DLL) {
+                    if (string.Equals(Path.GetFullPath(mod.label.install_path), currentPath)) {
+                        string modTitle = mod.label.title;
+
+                        for (int j = 0; j < ___entryParent.childCount; ++j) {
+                            Transform modSpecificTransform = ___entryParent.GetChild(j);
+                            HierarchyReferences hierarchyReferences = modSpecificTransform.GetComponent<HierarchyReferences>();
+                            LocText titleReference = hierarchyReferences.GetReference<LocText>("Title");
+
+                            if (titleReference != null && titleReference.text == modTitle) {
+                                titleReference.text = "<align=left>" + titleReference.text + " <line-height=0.000000001>\n<align=right>(" + Assembly.GetExecutingAssembly().GetName().Version + ")";
+                                titleReference.autoSizeTextContainer = false;
+
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
