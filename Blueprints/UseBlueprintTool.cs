79152b0e622b990d1a4b891d9944a8d54668e3d7
Mayall <KaylemWood@outlook.com>
2019-10-13 14:45:29 +0100

    Added folders. Added grid compositor major view.

--- Blueprints/UseBlueprintTool.cs
+++ Blueprints/UseBlueprintTool.cs
@@ -51,6 +51,10 @@ namespace Blueprints {
             gameObject.GetComponent<UseBlueprintToolInput>().ParentTool = this;
             ToolMenu.Instance.PriorityScreen.Show(true);
 
+            if (BlueprintsState.HasBlueprints()) {
+                GridCompositor.Instance.ToggleMajor(true);
+            }
+
             if (Input.GetKey(BlueprintsAssets.BLUEPRINTS_KEYBIND_USE_RELOAD) || Input.GetKeyUp(BlueprintsAssets.BLUEPRINTS_KEYBIND_USE_RELOAD)) {
                 int oldBlueprintCount = BlueprintsState.LoadedBlueprints.Count;
                 Utilities.ReloadBlueprints(true);
@@ -59,7 +63,7 @@ namespace Blueprints {
                 PopFXManager.Instance.SpawnFX(BlueprintsAssets.BLUEPRINTS_CREATE_ICON_SPRITE, string.Format(Strings.Get(BlueprintsStrings.STRING_BLUEPRINTS_USE_LOADEDBLUEPRINTS), Mathf.Abs(blueprintCountDelta) + " " + (blueprintCountDelta >= 0 ? Strings.Get(BlueprintsStrings.STRING_BLUEPRINTS_USE_LOADEDBLUEPRINTS_ADDITIONAL) : Strings.Get(BlueprintsStrings.STRING_BLUEPRINTS_USE_LOADEDBLUEPRINTS_FEWER)), BlueprintsState.LoadedBlueprints.Count), null, PlayerController.GetCursorPos(KInputManager.GetMousePos()), BlueprintsAssets.BLUEPRINTS_CONFIG_FXTIME * 4);
             }
 
-            if (BlueprintsState.LoadedBlueprints.Count > 0) {
+            if (BlueprintsState.HasBlueprints()) {
                 BlueprintsState.VisualizeBlueprint(Grid.PosToXY(PlayerController.GetCursorPos(KInputManager.GetMousePos())), BlueprintsState.SelectedBlueprint);
                 if (visualizer != null) {
                     Destroy(visualizer);
@@ -81,6 +85,7 @@ namespace Blueprints {
 
             BlueprintsState.ClearVisuals();
             ToolMenu.Instance.PriorityScreen.Show(false);
+            GridCompositor.Instance.ToggleMajor(false);
         }
 
         public override void OnLeftClickDown(Vector3 cursorPos) {
