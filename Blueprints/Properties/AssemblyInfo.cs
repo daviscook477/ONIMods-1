1cf14e220c96e6917dcc05fe2455659690bfce3c
Mayall <KaylemWood@outlook.com>
2019-10-15 11:38:43 +0100

    Incremented version.

--- Blueprints/Properties/AssemblyInfo.cs
+++ Blueprints/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Blueprints")]
 [assembly: AssemblyCopyright("Copyright Mayall 2019")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.2.51.0")]
+[assembly: AssemblyVersion("2.2.56.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
