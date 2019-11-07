0ce1243c37ab4840643d1ba483eb7a3b51caf21a
Mayall <KaylemWood@outlook.com>
2019-11-07 00:36:41 +0000

    Incremented version.

--- Pliers/Properties/AssemblyInfo.cs
+++ Pliers/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Pliers")]
 [assembly: AssemblyCopyright("Copyright Mayall 2019")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.50.0")]
+[assembly: AssemblyVersion("1.1.51.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
