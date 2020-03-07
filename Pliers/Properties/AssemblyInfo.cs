498c79f941f4601a14807755f8a038f274f42d32
Mayall <KaylemWood@outlook.com>
2020-03-07 10:57:16 +0000

    Updated copyright date.

--- Pliers/Properties/AssemblyInfo.cs
+++ Pliers/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Pliers")]
 [assembly: AssemblyProduct("Pliers")]
-[assembly: AssemblyCopyright("Copyright Mayall 2019")]
+[assembly: AssemblyCopyright("Copyright Mayall 2020")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.1.66.0")]
 [assembly: NeutralResourcesLanguage("en")]
